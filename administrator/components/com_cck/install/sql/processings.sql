99a0d32f05c8924f8fedf32974e4cd02854cccd2
Sebastien Heraud <sebastien@heraud.me>
2014-11-27 11:17:57 +0100

    More processings to Core (3)

---
+++ administrator/components/com_cck/install/sql/processings.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE IF NOT EXISTS `#__cck_more_processings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `description` varchar(5120) NOT NULL,
+  `ordering` int(11) NOT NULL DEFAULT '0',
+  `published` tinyint(3) NOT NULL DEFAULT '0',
+  `scriptfile` text NOT NULL,
+  `checked_out` int(10) unsigned NOT NULL DEFAULT '0',
+  `checked_out_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=50 ;
