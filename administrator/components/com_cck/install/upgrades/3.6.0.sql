5372681fa4544ce9290cc8d4d666e7c18c325db0
Sebastien Heraud <sebastien@heraud.me>
2015-02-10 12:03:23 +0100

    Complete SQL update file

--- administrator/components/com_cck/install/upgrades/3.6.0.sql
+++ administrator/components/com_cck/install/upgrades/3.6.0.sql
@@ -1,2 +1,4 @@
 
 UPDATE `#__cck_core_fields` SET `options` = 'Hide=-2||Standard=optgroup||Above=-1||Below=0||Both=1||Infinite=optgroup||Click=2' WHERE `id` = 244;
+
+UPDATE `#__cck_core_fields` SET `published` = 1 WHERE `id` IN (508,509,510,511,512,513,514,515,516);
