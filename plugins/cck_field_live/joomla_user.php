50892bf41e27f96003955bc85f8d3d017f3ff124
lionelratel <lionel.ratel@octopoos.com>
2015-02-12 10:36:39 +0100

    Get Live value from user with storage JSON

---
+++ plugins/cck_field_live/joomla_user.php
@@ -0,0 +1,83 @@
+<?php
+/**
+* @version 			SEBLOD 3.x Core
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2013 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Plugin
+class plgCCK_Field_LiveJoomla_User extends JCckPluginLive
+{
+	protected static $type	=	'joomla_user';
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+	
+	// onCCK_Field_LivePrepareForm
+	public function onCCK_Field_LivePrepareForm( &$field, &$value = '', &$config = array() )
+	{
+		if ( self::$type != $field->live ) {
+			return;
+		}
+		
+		// Init
+		$live		=	'';
+		$options	=	parent::g_getLive( $field->live_options );
+
+		// Prepare
+		$excluded	=	$options->get( 'excluded' );
+		$property	=	$options->get( 'property' );
+
+		if ( $property ) {
+			$user	=	JCck::getUser();
+			if ( $user->id > 0 && $user->guest == 1 ) {
+				if ( !( $property == 'ip' || $property == 'session_id' ) ) {
+					$user	=	new JUser( 0 );
+				}
+			}
+			if ( $property == 'access' ) {
+				$viewlevels	=	$user->getAuthorisedViewLevels();
+				if ( $excluded != '' ) {
+					$excluded	=	explode( ',', $excluded );
+					$viewlevels	=	array_diff( $viewlevels, $excluded );
+				}
+				if ( empty( $viewlevels ) ) {
+					$live	=	$options->get( 'default_value', '' );
+				} else {
+					$live	=	implode( ',', $viewlevels );	
+				}
+			} else {
+				if ( strpos( $property, '[' ) !== false ) {
+					$prop		= 	explode( '[', $property );
+					$property 	=	$prop[0];	
+				}
+				if ( isset( $user->$property ) ) {
+					$live		=	$user->$property;
+					if ( is_array( $live ) ) {
+						if ( $excluded != '' ) {
+							$excluded	=	explode( ',', $excluded );
+							$live		=	array_diff( $live, $excluded );
+						}
+						if ( empty( $live ) ) {
+							$live	=	$options->get( 'default_value', '' );
+						} else {
+							$live	=	implode( ',', $live );	
+						}
+					}
+					if ( isset( $prop ) ) {
+						$prop[0] 	= 	json_decode( $live, true );
+						$live 		= 	$prop[0][substr( $prop[1], 0, -1 )];
+					}
+				}
+			}
+		}
+		
+		// Set
+		$value	=	(string)$live;
+	}
+}
+?>
