8342cea18f13bdeb43196e4971534a22afa5c7bc
Sebastien Heraud <sebastien@heraud.me>
2015-07-16 14:52:45 +0200

    Add Session Manager

---
+++ administrator/components/com_cck/views/sessions/tmpl/default_filter.php
@@ -0,0 +1,50 @@
+<?php
+/**
+* @version 			SEBLOD 3.x Core ~ $Id: default_filter.php sebastienheraud $
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2013 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+$clear	=	"";
+if ( $this->js['filter'] ) {
+	$doc->addScriptDeclaration( $this->js['filter'] );
+}
+if ( $listDir == 'asc' ) {
+	$selected_asc	=	' selected="selected"';
+	$selected_desc	=	'';
+} else {
+	$selected_asc	=	'';
+	$selected_desc	=	' selected="selected"';
+}
+?>
+
+<div class="<?php echo $this->css['filter']; ?>" id="filter-bar">
+	<div class="<?php echo $this->css['filter_search']; ?>">
+        <?php
+		echo JCckDev::getForm( $cck['core_session_location_filter'], $this->state->get( 'filter.location' ), $config );
+		echo JCckDev::getForm( $cck['core_filter_input'], $this->escape( $this->state->get( 'filter.search' ) ), $config, array( 'attributes'=>'placeholder="'.JText::_( 'COM_CCK_ITEMS_SEARCH_FILTER' ).'" style="text-align:center;"' ), array( 'after'=>"\n" ) );
+		echo JCckDev::getForm( $cck['core_filter_go'], '', $config, array( 'css'=>$this->css['filter_search_button'] ), array( 'after'=>"\n" ) );
+		echo JCckDev::getForm( $cck['core_filter_search'], '', $config, array( 'css'=>$this->css['filter_search_button'], 'attributes'=>'onclick="'.$clear.'this.form.submit();"' ), array( 'after'=>"\n" ) );
+		echo JCckDev::getForm( $cck['core_filter_clear'], '', $config, array( 'css'=>$this->css['filter_search_button'], 'attributes'=>'onclick="document.getElementById(\'filter_search\').value=\'\';document.getElementById(\'filter_location\').value=\'title\';'.$clear.'this.form.submit();"' ) );
+		?>
+	</div>
+	<?php if ( JCck::on() ) { ?>
+		<div class="<?php echo $this->css['filter_search_list']; ?>">
+			<label for="limit" class="element-invisible"><?php echo JText::_( 'JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC' ); ?></label>
+			<select name="sortTable" id="sortTable" class="inputbox select input-medium" onchange="Joomla.orderTable()">
+				<option value=""><?php echo JText::_( 'JGLOBAL_SORT_BY' );?></option>
+				<?php echo JHtml::_( 'select.options', $this->getSortFields(), 'value', 'text', $listOrder ); ?>
+			</select>
+			<select name="directionTable" id="directionTable" class="inputbox select input-medium" onchange="Joomla.orderTable()">
+				<option value="asc"<?php echo $selected_asc; ?>><?php echo JText::_( 'JGLOBAL_ORDER_ASCENDING' ); ?></option>
+				<option value="desc"<?php echo $selected_desc; ?>><?php echo JText::_( 'JGLOBAL_ORDER_DESCENDING' );  ?></option>
+			</select>
+			<?php echo $this->pagination->getLimitBox(); ?>
+		</div>
+	<?php } ?>
+</div>
