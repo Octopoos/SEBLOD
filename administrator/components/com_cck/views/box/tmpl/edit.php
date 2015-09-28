e3a6e67e9833676db3f711773984a3b35326b792
David Beuving <david@khoza.com>
2015-09-28 14:27:35 -0700

    Adding a semi-colon to the end of the JCck.Dev.Close function declaration to fix javascript error

--- administrator/components/com_cck/views/box/tmpl/edit.php
+++ administrator/components/com_cck/views/box/tmpl/edit.php
@@ -75,7 +75,7 @@ if ( $this->doValidation == 1 ) {
 }
 $js		=	'if("undefined"===typeof JCck.Dev){JCck.Dev={}};'
 		.	'JCck.Dev.close = function() { if (parent.jQuery.fn.colorbox) {parent.jQuery.fn.colorbox.close();}'
-		.	'else {if (window.parent.SqueezeBox) {window.parent.SqueezeBox.close();};} }';
+		.	'else {if (window.parent.SqueezeBox) {window.parent.SqueezeBox.close();};} };';
 $doc->addScriptDeclaration( $js );
 ?>
 </form>
