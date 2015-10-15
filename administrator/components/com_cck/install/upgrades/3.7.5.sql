8d04879411f79309a18e5ff6a2adf765e48aaf3d
Sebastien Heraud <sebastien@heraud.me>
2015-10-15 20:23:47 +0200

    Update SQL files

---
+++ administrator/components/com_cck/install/upgrades/3.7.5.sql
@@ -0,0 +1,5 @@
+
+UPDATE `#__cck_core_fields` SET `options` = 'All Items=0||Standard=optgroup||1||2||3||4||5||6||8||9||10||12||15||20||25||30||50||100||Advanced=optgroup||200||300||400||500||600||700||800||900||1000||2000||3000||4000||5000||endgroup||Use Native=-1' WHERE `id` = 172;
+UPDATE `#__cck_core_fields` SET `options` = 'Hide=-2||Standard=optgroup||Above=-1||Below=0||Both=1||Infinite=optgroup||Click=2||Load=8' WHERE `id` = 244;
+
+-- --------
