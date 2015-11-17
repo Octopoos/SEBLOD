b5297ad37c5da72eb4355d37179ee814c55764a4
Sebastien Heraud <sebastien@heraud.me>
2015-11-17 09:30:52 +0100

    Quick Fix

--- plugins/cck_field/storage/tmpl/form.php
+++ plugins/cck_field/storage/tmpl/form.php
@@ -74,6 +74,9 @@ $js		=	'
 					$("#storage_table").hide();
 				}
 				$("#op-"+v).show();
+				if ($("#storage").val() == "none"){
+					$(".storage-cck-more").attr("disabled","disabled"); $(".storage-cck-more").parent().hide();
+				}
 				if ($("#jform_id").val()==0){
 					if (parent.jQuery("#element").length && parent.jQuery("#element").val() == "type") {
 						$(".storage-cck-more").parent().remove();
