bc383074164bcf7ac4837d25b74aa0366a86ab02
Sebastien Heraud <sebastien@heraud.me>
2016-01-12 11:51:37 +0100

    Fix "Cyrillic" issue when saving fields

--- administrator/components/com_cck/helpers/helper_workshop.php
+++ administrator/components/com_cck/helpers/helper_workshop.php
@@ -19,7 +19,7 @@ class Helper_Workshop
 	public static function displayField( &$field, $type_field = '' )
 	{
 		$link	=	'index.php?option=com_cck&task=field.edit&id='.$field->id.'&tmpl=component';
-		?><li class="field <?php echo 't-'.$field->type.' f-'.$field->folder.' a-'. strtolower( substr( $field->title, 0, 1 ) ).$type_field; ?>" id="<?php echo $field->id; ?>"><a class="cbox edit" href="<?php echo $link; ?>"></a><span class="title" onDblClick="JCck.Dev.moveDir('<?php echo $field->id; ?>');"><?php echo $field->title; ?><span class="subtitle">(<?php echo JText::_( 'PLG_CCK_FIELD_'.$field->type.'_LABEL2' ); ?>)</span></span><input type="hidden" id="k<?php echo $field->id; ?>" name="ff[<?php echo $field->name; ?>]" value="<?php echo $field->id; ?>" /><?php echo '<div class="move" onClick="JCck.Dev.moveDir('.$field->id.');"></div>'; ?><div class="drag"></div><?php echo @$field->params; ?></li><?php
+		?><li class="field <?php echo 't-'.$field->type.' f-'.$field->folder.' a-'.mb_convert_case( substr( $field->title, 0, 1 ), MB_CASE_LOWER, 'UTF-8' ).$type_field; ?>" id="<?php echo $field->id; ?>"><a class="cbox edit" href="<?php echo $link; ?>"></a><span class="title" onDblClick="JCck.Dev.moveDir('<?php echo $field->id; ?>');"><?php echo $field->title; ?><span class="subtitle">(<?php echo JText::_( 'PLG_CCK_FIELD_'.$field->type.'_LABEL2' ); ?>)</span></span><input type="hidden" id="k<?php echo $field->id; ?>" name="ff[<?php echo $field->name; ?>]" value="<?php echo $field->id; ?>" /><?php echo '<div class="move" onClick="JCck.Dev.moveDir('.$field->id.');"></div>'; ?><div class="drag"></div><?php echo @$field->params; ?></li><?php
 	}
 	
 	// displayHeader
