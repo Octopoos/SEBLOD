f85d80e9c06f3e3576ca1b4b5bdf89de7ed35776
Sebastien Heraud <sebastien@heraud.me>
2016-04-21 16:49:42 +0200

    Patch website for fields created between 533 and 5000 (InnoDB reset of auto_inc)

--- administrator/components/com_cck/script.php
+++ administrator/components/com_cck/script.php
@@ -130,11 +130,83 @@ class com_cckInstallerScript
 					}
 				}
 				if ( $errors ) {
-					JFactory::getApplication()->enqueueMessage( 'Patch IDs failed.. while updating to SEBLOD 3.2' );
+					JFactory::getApplication()->enqueueMessage( 'Patch IDs failed.. while updating to SEBLOD 3.2.0' );
 				}
 			}
 		}
 		// -- End
+
+		// -- Patch for websites started between SEBLOD 3.6.0+ and 3.8.0-
+		if ( (float)$app->cck_core_version_old >= 3.6 && (float)$app->cck_core_version_old < 3.8 ) {
+			$db			=	JFactory::getDbo();
+			$db->setQuery( 'SELECT id, name FROM #__cck_core_fields WHERE id >= 533 AND id < 5000' );
+			$fields		=	$db->loadObjectList();
+			$ignore		=	array(
+								'533'=>array( 'name'=>'core_session_extension', 'sql'=>"(533, 'Core Session Extension', 'core_session_extension', 3, 'select_simple', '', 0, 'Extension', 'Select', 3, 'required', '', '', 'Exporter=com_cck_exporter||Importer=com_cck_importer', '', 0, 255, 32, 0, 0, 0, 0, '', 0, '', '', '', '', 0, 0, 0, 0, 0, 0, 1, 'max-width-150', '', 'dev', '', '', '', 'extension', '', '', '', 0, '0000-00-00 00:00:00')" ),
+								'534'=>array( 'name'=>'core_session_location_filter', 'sql'=>"(534, 'Core Session Location Filter', 'core_session_location_filter', 3, 'select_simple', '', 0, 'Location', ' ', 3, '', '', '', 'Title=title||Name=name||IDS=id', '', 0, 255, 32, 0, 0, 0, 0, '', 0, '', '', '', '', 0, 0, 0, 0, 0, 0, 1, '', '', 'dev', '', '', '', 'filter_location', '', '', '', 0, '0000-00-00 00:00:00')" ),
+								'535'=>array( 'name'=>'tab2_details', 'sql'=>"(535, 'Tab2 Details (Start)', 'tab2_details', 3, 'tabs', '', 1, 'Details', '', 3, '', '', '', '', '', 0, 255, 32, 0, 0, 0, 0, '', 0, 'cck_tabs2', '', '', '', 0, 1, 0, 0, 0, 0, 1, '', '', 'none', '', '', '', 'tab2_details', '', '', '', 0, '0000-00-00 00:00:00')" ),
+								'536'=>array( 'name'=>'tab2_publishing', 'sql'=>"(536, 'Tab2 Publishing (Panel)', 'tab2_publishing', 3, 'tabs', '', 1, 'Publishing', '', 3, '', '', '', '', '', 0, 255, 32, 0, 0, 0, 0, '', 1, 'cck_tabs2', '', '', '', 0, 0, 0, 0, 0, 0, 1, '', '', 'none', 'seb_session', '', '', 'tab2_publishing', '', '', '', 0, '0000-00-00 00:00:00')" ),
+								'537'=>array( 'name'=>'tab2_end', 'sql'=>"(537, 'Tab2 (End)', 'tab2_end', 3, 'tabs', '', 1, 'clear', '', 3, '', '', '', '', '', 0, 255, 32, 0, 0, 0, 0, '', 2, 'cck_tabs2', '', '', '', 0, 0, 0, 0, 0, 0, 1, '', '', 'none', 'seb_session', '', '', 'tab2_end', '', '', '', 0, '0000-00-00 00:00:00')" ),
+								'538'=>array( 'name'=>'tab2_metadata', 'sql'=>"(538, 'Tab2 Metadata (Panel)', 'tab2_metadata', 3, 'tabs', '', 1, 'Metadata', '', 3, '', '', '', '', '', 0, 255, 32, 0, 0, 0, 0, '', 1, 'cck_tabs2', '', '', '', 0, 0, 0, 0, 0, 0, 1, '', '', 'none', 'seb_session', '', '', 'tab2_metadata', '', '', '', 0, '0000-00-00 00:00:00')" ),
+								'539'=>array( 'name'=>'tab2_options', 'sql'=>"(539, 'Tab2 Options (Panel)', 'tab2_options', 3, 'tabs', '', 1, 'Options', '', 3, '', '', '', '', '', 0, 255, 32, 0, 0, 0, 0, '', 1, 'cck_tabs2', '', '', '', 0, 0, 0, 0, 0, 0, 1, '', '', 'none', 'seb_session', '', '', 'tab2_options', '', '', '', 0, '0000-00-00 00:00:00')" ),
+								'540'=>array( 'name'=>'tab2_media', 'sql'=>"(540, 'Tab2 Media (Panel)', 'tab2_media', 3, 'tabs', '', 1, 'Media', '', 3, '', '', '', '', '', 0, 255, 32, 0, 0, 0, 0, '', 1, 'cck_tabs2', '', '', '', 0, 0, 0, 0, 0, 0, 1, '', '', 'none', 'seb_session', '', '', 'tab2_media', '', '', '', 0, '0000-00-00 00:00:00')" ),
+								'541'=>array( 'name'=>'tab2_permissions', 'sql'=>"(541, 'Tab2 Permissions (Panel)', 'tab2_permissions', 3, 'tabs', '', 1, 'Permissions', '', 3, '', '', '', '', '', 0, 255, 32, 0, 0, 0, 0, '', 1, 'cck_tabs2', '', '', '', 0, 0, 0, 0, 0, 0, 1, '', '', 'none', 'seb_session', '', '', 'tab2_permissions', '', '', '', 0, '0000-00-00 00:00:00')" )
+							);
+			$redo		=	array();
+			if ( count( $fields ) ) {
+				$errors	=	0;
+				
+				foreach ( $fields as $f ) {
+					$id		=	(int)$f->id;
+					$id2	=	(int)'110'.$id;
+
+					if ( isset( $ignore[$f->id] ) && $ignore[$f->id]['name'] == $f->name ) {
+						continue;
+					}
+					$query	=	'UPDATE #__cck_core_fields SET id = '.$id2.' WHERE id = '.(int)$id;
+					$db->setQuery( $query );
+
+					if ( $db->execute() !== false ) {
+						$query	=	'UPDATE #__cck_core_type_field SET fieldid = '.$id2.' WHERE fieldid = '.$id;
+						$db->setQuery( $query );
+						$db->execute();
+
+						$query	=	'UPDATE #__cck_core_search_field SET fieldid = '.$id2.' WHERE fieldid = '.$id;
+						$db->setQuery( $query );
+						$db->execute();
+
+						$redo[(string)$f->id]	=	true;
+					} else {
+						$errors++;
+					}
+				}
+				if ( ( $nb = count( $redo ) ) ) {
+					if ( version_compare( $app->cck_core_version_old, '3.6.5', '>' ) ) {
+						foreach ( $redo as $f_id=>$f_val ) {
+							if ( $f_id == 533 || $f_id == 534 ) {
+								$do	=	true;
+							} elseif ( ( $f_id >= 535 && $f_id <= 541 ) && version_compare( $app->cck_core_version_old, '3.7.2', '>=' ) ) {
+								$do	=	true;
+							} else {
+								$do	=	false;
+							}
+							if ( !$do ) {
+								continue;
+							}
+							if ( isset( $ignore[$f_id] ) && $ignore[$f_id]['sql'] != '' ) {
+								$query	=	'INSERT IGNORE INTO `#__cck_core_fields` (`id`, `title`, `name`, `folder`, `type`, `description`, `published`, `label`, `selectlabel`, `display`, `required`, `validation`, `defaultvalue`, `options`, `options2`, `minlength`, `maxlength`, `size`, `cols`, `rows`, `ordering`, `sorting`, `divider`, `bool`, `location`, `extended`, `style`, `script`, `bool2`, `bool3`, `bool4`, `bool5`, `bool6`, `bool7`, `bool8`, `css`, `attributes`, `storage`, `storage_cck`, `storage_location`, `storage_table`, `storage_field`, `storage_field2`, `storage_params`, `storages`, `checked_out`, `checked_out_time`) VALUES '.$ignore[$f_id]['sql'];
+								$db->setQuery( $query );
+								$db->execute();
+							}
+						}
+					}
+					JFactory::getApplication()->enqueueMessage( '<strong>'.$nb.' field(s)</strong> patched (>= 533 and < 5000).' );
+				}
+				if ( $errors ) {
+					JFactory::getApplication()->enqueueMessage( 'Patch IDs failed.. while updating to SEBLOD 3.8.0', 'error' );
+				}
+			}
+		}
 	}
 	
 	// preflight
