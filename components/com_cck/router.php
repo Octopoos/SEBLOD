1d30f87e69d3acfaae9e9f684be4dd665ae855c6
Sebastien Heraud <sebastien@heraud.me>
2016-05-22 11:37:49 +0200

    Fix 2 segment routes when the Child Item is a SEBLOD List

--- components/com_cck/router.php
+++ components/com_cck/router.php
@@ -131,7 +131,19 @@ class CckRouter extends JComponentRouterBase
 
 							if ( $properties[$target] != '' ) {
 								$params['doSEF'][0]	=	'2';
-								$params['location']	=	$properties[$target];
+								$isNew			=	true;
+
+								$parent_id		=	$menuItem->parent_id;
+								$parent			=	$menu->getItem( $parent_id );
+
+								if ( is_object( $parent ) ) {
+									if ( $parent->query['option'] == 'com_cck' && $parent->query['view'] == 'list' ) {
+										$isNew	=	false;
+									}
+								}
+								if ( $isnew ) {
+									$params['location']	=	$properties[$target];
+								}
 							}
 						}
 					}
