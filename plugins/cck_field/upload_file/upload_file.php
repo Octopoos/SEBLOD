172630fa7bb5ca5ec32e87e78d41bbf1fd2a768d
Sebastien Heraud <sebastien@heraud.me>
2016-07-06 11:42:37 +0200

    Quick updates

--- plugins/cck_field/upload_file/upload_file.php
+++ plugins/cck_field/upload_file/upload_file.php
@@ -709,7 +709,7 @@ class plgCCK_FieldUpload_File extends JCckPluginField
 	// _getHits
 	protected static function _getHits( $id, $fieldname, $collection = '', $x = 0 )
 	{
-		$query	= 'SELECT a.hits FROM #__cck_core_downloads AS a WHERE a.id = '.(int)$id.' AND a.field = "'.(string)$fieldname.'" AND a.collection = "'.(string)$collection.'" AND a.x = '.(int)$x;
+		$query	=	'SELECT a.hits FROM #__cck_core_downloads AS a WHERE a.id = '.(int)$id.' AND a.field = "'.(string)$fieldname.'" AND a.collection = "'.(string)$collection.'" AND a.x = '.(int)$x;
 		$hits	=	JCckDatabase::loadResult( $query ); //@
 		
 		return ( $hits ) ? $hits : 0;
