819d92c5fad7a8e9f78cea2fcaa6262071578dea
Sebastien Heraud <sebastien@heraud.me>
2016-07-21 14:11:24 +0200

    Quick Fix

--- modules/mod_cck_list/mod_cck_list.php
+++ modules/mod_cck_list/mod_cck_list.php
@@ -23,11 +23,11 @@ $app	=	JFactory::getApplication();
 $uniqId	=	'm'.$module->id;
 $formId	=	'seblod_form_'.$uniqId;
 
-if ( ! defined ( 'JPATH_LIBRARIES_CCK' ) ) {
-	define( 'JPATH_LIBRARIES_CCK',	JPATH_SITE.'/libraries/cck' );
+if ( ! defined( 'JPATH_LIBRARIES_CCK' ) ) {
+	define( 'JPATH_LIBRARIES_CCK', JPATH_SITE.'/libraries/cck' );
 }
-if ( ! defined ( 'JROOT_MEDIA_CCK' ) ) {
-	define( 'JROOT_MEDIA_CCK',	JUri::root( true ).'/media/cck' );
+if ( ! defined( 'JROOT_MEDIA_CCK' ) ) {
+	define( 'JROOT_MEDIA_CCK', JUri::root( true ).'/media/cck' );
 }
 JCck::loadjQuery();
 JFactory::getLanguage()->load( 'com_cck_default', JPATH_SITE );
