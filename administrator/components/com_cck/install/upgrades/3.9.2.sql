08c9e055567f8c6551850a0845518552a95d2fb3
Sebastien Heraud <sebastien@heraud.me>
2016-09-27 19:44:16 +0200

    Update SQL files

--- administrator/components/com_cck/install/upgrades/3.9.2.sql
+++ administrator/components/com_cck/install/upgrades/3.9.2.sql
@@ -1,8 +1,2 @@
 
-ALTER TABLE `#__cck_core_types` ADD `admin_form` TINYINT(3) NOT NULL DEFAULT '0' AFTER `options_intro`;
-
 ALTER TABLE `#__cck_core_templates` ADD `options` VARCHAR(2048) NOT NULL AFTER `featured`;
-
-ALTER TABLE `#__cck_core_versions` ADD `featured` TINYINT(3) NOT NULL DEFAULT '0' AFTER `user_id`;
-
-UPDATE `#__cck_core_fields` SET `maxlength` = 69 WHERE `id` = 260;
