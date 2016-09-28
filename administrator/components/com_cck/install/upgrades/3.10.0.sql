978743de251b46947d86b2efaeb55e74a2f0b4cd
Sebastien Heraud <sebastien@heraud.me>
2016-09-28 11:44:29 +0200

    Fix SQL query for upcoming update

--- administrator/components/com_cck/install/upgrades/3.10.0.sql
+++ administrator/components/com_cck/install/upgrades/3.10.0.sql
@@ -3,4 +3,4 @@ ALTER TABLE `#__cck_core_types` ADD `admin_form` TINYINT(3) NOT NULL DEFAULT '0'
 
 ALTER TABLE `#__cck_core_versions` ADD `featured` TINYINT(3) NOT NULL DEFAULT '0' AFTER `user_id`;
 
-UPDATE `#__cck_core_fields` SET `maxlength` = 69 WHERE `id` = 260;
+UPDATE `#__cck_core_fields` SET `size` = 69 WHERE `id` = 260;
