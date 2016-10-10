3f8cd1877923d074d3fee760278f57fb96f354ab
Sebastien Heraud <sebastien@heraud.me>
2016-10-10 11:08:59 +0200

    Quick Fix

--- administrator/components/com_cck/install/upgrades/3.10.1.sql
+++ administrator/components/com_cck/install/upgrades/3.10.1.sql
@@ -1,2 +1,4 @@
 
 UPDATE `#__cck_core_fields` SET `bool4` = 2, `options` = 'Aka/Join Tables for Search=optgroup||Placeholder Table1=aka_table1||Placeholder Table2=aka_table2||Placeholder Table3=aka_table3' WHERE `id` = 29;
+
+UPDATE `#__cck_core_fields` SET `options` = REPLACE( `options`, 'Prepend Segment', 'Append Segment' ) WHERE `id` = 177;
