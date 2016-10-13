407ef4631021c87394ca0961c51d74ef898afff9
Sebastien Heraud <sebastien@heraud.me>
2016-10-13 18:45:27 +0200

    Remove notice on multi-site

--- plugins/system/cck/cck.php
+++ plugins/system/cck/cck.php
@@ -446,10 +446,16 @@ class plgSystemCCK extends JPlugin
 					$this->offline_buffer	=	$doc->render( false, $params );
 				} elseif ( $this->site_cfg->get( 'set_template_style', false ) ) {
 					$menu	=	$app->getMenu();
+					
 					if ( is_object( $menu ) ) {
-						$style	=	$menu->getActive()->template_style_id;
-						if ( $style ) {
-							$this->_setTemplateStyle( $style );
+						$active	=	$menu->getActive();
+						
+						if ( is_object( $active ) ) {
+							$style	=	$active->template_style_id;
+							
+							if ( $style ) {
+								$this->_setTemplateStyle( $style );
+							}
 						}
 					}
 				}
