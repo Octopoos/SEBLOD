22ba56fd59d4b2cdeae96a1b5b1ca45dcb086689
Sebastien Heraud <sebastien@heraud.me>
2017-04-14 16:02:01 +0200

    Update Copyright

--- libraries/cck/development/plugins/plg_cck_field_typo/install/script.php
+++ libraries/cck/development/plugins/plg_cck_field_typo/install/script.php
@@ -2,7 +2,7 @@
 /**
 * @version 			SEBLOD 3.x More
 * @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
-* @url				http://www.seblod.com
+* @url				https://www.seblod.com
 * @editor			Octopoos - www.octopoos.com
 * @copyright		Copyright (C) 2009 - 2016 SEBLOD. All Rights Reserved.
 * @license 			GNU General Public License version 2 or later; see _LICENSE.php
