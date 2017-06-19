a6b01b94b520e612b36f6fb6fc5b974ceee1e00c
Sebastien Heraud <sebastien@heraud.me>
2017-06-19 15:05:06 +0200

    Improve "Dropdown", "Featured", "Status" behaviours

--- plugins/cck_field_typo/joomla_jgrid/joomla_jgrid.php
+++ plugins/cck_field_typo/joomla_jgrid/joomla_jgrid.php
@@ -98,20 +98,32 @@ class plgCCK_Field_TypoJoomla_Jgrid extends JCckPluginTypo
 				$config['formWrapper']	=	true;
 				break;
 			case 'dropdown':
-				$class	=	$typo->get( 'class1', '' );
-				$class	=	$class ? ' '.$class : '';
-				$value	=	'<button data-toggle="dropdown" class="dropdown-toggle btn'.$class.'"><span class="caret"></span></button>'
-						.	'<ul class="dropdown-menu">'
-						.	'<li>'
-						.	( ( isset( $field->html ) && $field->html ) ? $field->html : $value )
-						.	'</li>'
-						.	'</ul>';
+				static $dropdown		=	array();
+				static $dropdown_css	=	false;
+
+				if ( !isset( $dropdown[$pk] ) ) {
+					$class	=	$typo->get( 'class1', '' );
+					$class	=	$class ? ' '.$class : '';
+					$value	=	'<button data-toggle="dropdown" class="dropdown-toggle btn'.$class.'"><span class="caret"></span></button>'
+							.	'<ul class="dropdown-menu flex-column-reverse"></ul>';
+
+					$dropdown[$pk]		=	array( 'parent'=>$field->name, 'html'=>'' );
+
+					if ( !$dropdown_css ) {
+						$dropdown_css	=	true;
+
+						JFactory::getDocument()->addStyleDeclaration( '.btn-group.open > .dropdown-menu{display: -webkit-box; display: -ms-flexbox; display: flex; -webkit-box-orient:vertical; -webkit-box-direction:reverse; -ms-flex-direction:column-reverse; flex-direction:column-reverse;}' );
+					}
+				}
+				$dropdown[$pk]['html']	=	'<li>'.( ( isset( $field->html ) && $field->html ) ? $field->html : $value ).'</li>';
+
+				parent::g_addProcess( 'beforeRenderContent', self::$type, $config, array( 'name'=>$dropdown[$pk]['parent'], 'target'=>$field->name, 'type'=>$type, 'html'=>$dropdown[$pk]['html'] ) );
 				break;
 			case 'featured':
-				static $loadedF = 0;
-				if ( !$loadedF ) {
+				static $loaded_featured	=	0;
+				if ( !$loaded_featured ) {
 					JHtml::addIncludePath( JPATH_ADMINISTRATOR.'/components/com_content/helpers/html' );
-					$loadedF	=	1;
+					$loaded_featured	=	1;
 				}
 				$value		=	$field->value;
 
@@ -122,12 +134,8 @@ class plgCCK_Field_TypoJoomla_Jgrid extends JCckPluginTypo
 				} else {
 					$value	=	( $value == '' ) ? 0 : 1;
 				}
-				$value		=	JHtml::_( 'contentadministrator.featured', $value, $pks[$pk], false /*$canChange*/ );
 
-				if ( !( $class == '' || $class == 'btn btn-micro hasTooltip' ) ) {
-					$class	.=	' disabled';
-					$value	=	preg_replace( '#class="[a-zA-Z0-9\-\ ]*" #U', 'class="'.$class.'"', $value );
-				}
+				parent::g_addProcess( 'beforeRenderContent', self::$type, $config, array( 'name'=>$field->name, 'type'=>$type, 'value'=>$value, 'class'=>$class, 'pk'=>$pks[$pk] ) );
 
 				$config['formWrapper']	=	true;
 				break;
@@ -242,19 +250,7 @@ class plgCCK_Field_TypoJoomla_Jgrid extends JCckPluginTypo
 				$config['formWrapper']	=	true;
 				break;
 			case 'state':
-				$field_name_up		=	$typo->get( 'state_up', '' );
-				$field_name_down	=	$typo->get( 'state_down', '' );
-
-				if ( $field_name_up || $field_name_down ) {
-					parent::g_addProcess( 'beforeRenderContent', self::$type, $config, array( 'name'=>$field->name, 'value'=>$field->value, 'class'=>$class, 'pk'=>$pks[$pk], 'fieldname_up'=>$field_name_up, 'fieldname_down'=>$field_name_down ) );
-				} else {
-					$value		=	JHtml::_( 'jgrid.published', $field->value, $pks[$pk], '', false /*$canChange*/, 'cb', '', '' );
-					
-					if ( !( $class == '' || $class == 'btn btn-micro hasTooltip' ) ) {
-						$class	.=	' disabled';
-						$value	=	preg_replace( '#class="[a-zA-Z0-9\-\ ]*" #U', 'class="'.$class.'"', $value );
-					}
-				}
+				parent::g_addProcess( 'beforeRenderContent', self::$type, $config, array( 'name'=>$field->name, 'type'=>$type, 'value'=>$field->value, 'class'=>$class, 'pk'=>$pks[$pk], 'fieldname_up'=>$typo->get( 'state_up', '' ), 'fieldname_down'=>$typo->get( 'state_down', '' ) ) );
 
 				$config['formWrapper']	=	true;
 				break;
@@ -278,25 +274,71 @@ class plgCCK_Field_TypoJoomla_Jgrid extends JCckPluginTypo
 	// onCCK_Field_TypoBeforeRenderContent
 	public static function onCCK_Field_TypoBeforeRenderContent( $process, &$fields, &$storages, &$config = array() )
 	{
-		$class				=	$process['class'];
-		$name				=	$process['name'];
-		$field_name_up		=	$process['fieldname_up'];
-		$field_name_down	=	$process['fieldname_down'];
+		$name	=	$process['name'];
+		$type	=	$process['type'];
 
-		$state_up			=	( $field_name_up != '' && isset( $fields[$field_name_up] ) ) ? $fields[$field_name_up]->value : '';
-		$state_up			=	( $state_up == '' ) ? '0000-00-00 00:00:00' : $state_up;
-		$state_down			=	( $field_name_down != '' && isset( $fields[$field_name_down] ) ) ? $fields[$field_name_down]->value : '';
-		$state_down			=	( $state_down == '' ) ? '0000-00-00 00:00:00' : $state_down;
+		if ( !$name ) {
+			return;
+		}
 
-		if ( $name ) {
-			$value			=	JHtml::_( 'jgrid.published', $process['value'], $process['pk'], '', false /*$canChange*/, 'cb', $state_up, $state_down );
-			
+		if ( $type == 'state' ) {
+			$class				=	$process['class'];
+			$field_name_up		=	$process['fieldname_up'];
+			$field_name_down	=	$process['fieldname_down'];
+
+			if ( $field_name_up || $field_name_down ) {
+				$state_up		=	( $field_name_up != '' && isset( $fields[$field_name_up] ) ) ? $fields[$field_name_up]->value : '';
+				$state_up		=	( $state_up == '' ) ? '0000-00-00 00:00:00' : $state_up;
+				$state_down		=	( $field_name_down != '' && isset( $fields[$field_name_down] ) ) ? $fields[$field_name_down]->value : '';
+				$state_down		=	( $state_down == '' ) ? '0000-00-00 00:00:00' : $state_down;
+
+				$value			=	JHtml::_( 'jgrid.published', $process['value'], $process['pk'], '', false /*$canChange*/, 'cb', $state_up, $state_down );
+			} else {
+				$value			=	JHtml::_( 'jgrid.published', $process['value'], $process['pk'], '', false /*$canChange*/, 'cb', '', '' );
+			}
+			if ( $fields[$name]->link ) {
+				$hasLink		=	true;
+				$value			=	str_replace( '<a ', '<a href="'.$fields[$name]->link.'"', $value );
+			} else {
+				$hasLink		=	false;
+			}
 			if ( !( $class == '' || $class == 'btn btn-micro hasTooltip' ) ) {
-				$class		.=	' disabled';
+				if ( !$hasLink ) {
+					$class	.=	' disabled';
+				}
+				if ( isset( $fields[$name]->link_title ) && $fields[$name]->link_title ) {
+					$value	=	preg_replace( '#title=".*"#U', 'title="'.$fields[$name]->link_title.'"', $value );
+				}
 				$value		=	preg_replace( '#class="[a-zA-Z0-9\-\ ]*" #U', 'class="'.$class.'"', $value );
 			}
 
 			$fields[$name]->typo	=	$value;
+		} elseif ( $type == 'featured' ) {
+			$class		=	$process['class'];
+			$value		=	JHtml::_( 'contentadministrator.featured', $process['value'], $process['pk'], false /*$canChange*/ );
+
+			if ( $fields[$name]->link ) {
+				$hasLink		=	true;
+				$value			=	str_replace( '<a ', '<a href="'.$fields[$name]->link.'"', $value );
+			} else {
+				$hasLink		=	false;
+			}
+			if ( !( $class == '' || $class == 'btn btn-micro hasTooltip' ) ) {
+				if ( !$hasLink ) {
+					$class	.=	' disabled';
+				}
+				if ( isset( $fields[$name]->link_title ) && $fields[$name]->link_title ) {
+					$value	=	preg_replace( '#title=".*"#U', 'title="'.$fields[$name]->link_title.'"', $value );
+				}
+				$value	=	preg_replace( '#class="[a-zA-Z0-9\-\ ]*" #U', 'class="'.$class.'"', $value );
+			}
+			$fields[$name]->typo	=	$value;
+		} elseif ( $type == 'dropdown' ) {
+			$target	=	$process['target'];
+
+			if ( $fields[$target]->display ) {
+				$fields[$name]->typo	=	str_replace( '<ul class="dropdown-menu flex-column-reverse">', '<ul class="dropdown-menu flex-column-reverse">'.$process['html'], $fields[$name]->typo );
+			}
 		}
 	}
 }
