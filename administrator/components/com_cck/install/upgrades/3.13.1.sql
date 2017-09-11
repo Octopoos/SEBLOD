49f8bce7d29419f16c2e7a50eefd44c3427cf4db
Sebastien Heraud <sebastien@heraud.me>
2017-09-11 14:58:33 +0200

    Update SQL files

---
+++ administrator/components/com_cck/install/upgrades/3.13.1.sql
@@ -0,0 +1,4 @@
+
+ALTER TABLE `#__cck_core_sites` ADD `parent_id` INT(10) UNSIGNED NOT NULL AFTER `options`;
+
+UPDATE `#__cck_core_fields` SET `options` = 'Task Cancel=cancel||Task Reset=reset||Task Reset and Search=reset2save||Task Save=apply||Task Save and Close=save||Task Save and New=save2new||Task Save and Redirect=save2redirect||Task Save and Skip=save2skip||Task Save and View=save2view||Task Save as New=save2copy||SEBLOD Ecommerce Addon=optgroup||Task Save for later=save4later||SEBLOD Exporter Addon=optgroup||Task Export=export||Task Export Ajax=export_ajax||SEBLOD Toolbox Addon=optgroup||Task Process=process||Task Process Ajax=process_ajax' WHERE `id` = 486;
