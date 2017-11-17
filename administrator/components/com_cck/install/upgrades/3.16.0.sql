9f34c3ffb0aa267df84cc62b7e4d7855ec664a88
Sebastien Heraud <sebastien@heraud.me>
2017-11-17 10:49:28 +0100

    Update SQL files

--- administrator/components/com_cck/install/upgrades/3.16.0.sql
+++ administrator/components/com_cck/install/upgrades/3.16.0.sql
@@ -2,3 +2,5 @@
 ALTER TABLE `#__cck_core_types` ADD `parent_inherit` TINYINT(3) NOT NULL AFTER `parent`;
 
 UPDATE `#__cck_core_types` SET `parent_inherit`=1 WHERE `parent` != "";
+
+UPDATE `#__cck_core_fields` SET `options` = 'Allowed=||Allowed Hidden=hidden||Not Allowed=none||location=optgroup||Administrator Only=admin||Site Only=site' WHERE `id` = 276;
