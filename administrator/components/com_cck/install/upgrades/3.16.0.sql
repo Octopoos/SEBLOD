41d70880f4201a0f888cfc60ad1bc0837c2a0995
Sebastien Heraud <sebastien@heraud.me>
2017-12-04 12:25:35 +0100

    Update SQL files

--- administrator/components/com_cck/install/upgrades/3.16.0.sql
+++ administrator/components/com_cck/install/upgrades/3.16.0.sql
@@ -1,6 +1,8 @@
 
 ALTER TABLE `#__cck_core_types` ADD `parent_inherit` TINYINT(3) NOT NULL AFTER `parent`;
 
+ALTER TABLE `#__cck_core_searchs` ADD `sef_route_aliases` TINYINT(3) NOT NULL DEFAULT '0' AFTER `sef_route`;
+
 UPDATE `#__cck_core_types` SET `parent_inherit`=1 WHERE `parent` != "";
 
 UPDATE `#__cck_core_fields` SET `options` = 'Allowed=||Allowed Hidden=hidden||Not Allowed=none||location=optgroup||Administrator Only=admin||Site Only=site' WHERE `id` = 276;
