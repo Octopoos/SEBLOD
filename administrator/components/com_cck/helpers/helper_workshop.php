216a1ae493b7740b082629ae413de8119c0b5aed
Sebastien Heraud <sebastien@heraud.me>
2018-01-05 19:11:48 +0100

    Fix Back-end UI when !function_exists( 'mb_convert_case' )

--- administrator/components/com_cck/helpers/helper_workshop.php
+++ administrator/components/com_cck/helpers/helper_workshop.php
@@ -23,7 +23,7 @@ class Helper_Workshop
 		if ( $hasMb < 0 ) {
 			$hasMb	=	( function_exists( 'mb_convert_case' ) ) ? 1 : 0;
 		}
-		$name	=	( $hasMb ) ? mb_convert_case( substr( $field->title, 0, 1 ), MB_CASE_LOWER, 'UTF-8' ) : strtolower( $field->title );
+		$name	=	( $hasMb ) ? mb_convert_case( substr( $field->title, 0, 1 ), MB_CASE_LOWER, 'UTF-8' ) : strtolower( substr( $field->title, 0, 1 ) );
 		$link	=	'index.php?option=com_cck&task=field.edit&id='.$field->id.'&tmpl=component';
 		?><li class="field <?php echo 't-'.$field->type.' f-'.$field->folder.' a-'.$name.$type_field; ?>" id="<?php echo $field->id; ?>"><a class="cbox<?php echo $attr['class']; ?>" href="<?php echo $link; ?>"><?php echo $attr['span']; ?></a><span class="title" onDblClick="JCck.DevHelper.move('<?php echo $field->id; ?>');"><?php echo $field->title; ?><span class="subtitle">(<?php echo JText::_( 'PLG_CCK_FIELD_'.$field->type.'_LABEL2' ); ?>)</span></span><input type="hidden" id="k<?php echo $field->id; ?>" name="ff[<?php echo $field->name; ?>]" value="<?php echo $field->id; ?>" /><?php echo '<div class="move" onClick="JCck.DevHelper.move('.$field->id.');"></div>'; ?><div class="drag"></div><?php echo @$field->params; ?></li><?php
 	}
