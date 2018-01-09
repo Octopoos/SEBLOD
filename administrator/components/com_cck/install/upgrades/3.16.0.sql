8fac128ae62ff51051f2e8435721da26a0f63efe
Sebastien Heraud <sebastien@heraud.me>
2018-01-09 12:11:47 +0100

    Update SQL files

--- administrator/components/com_cck/install/upgrades/3.16.0.sql
+++ administrator/components/com_cck/install/upgrades/3.16.0.sql
@@ -3,6 +3,8 @@ ALTER TABLE `#__cck_core_types` ADD `parent_inherit` TINYINT(3) NOT NULL AFTER `
 
 ALTER TABLE `#__cck_core_searchs` ADD `sef_route_aliases` TINYINT(3) NOT NULL DEFAULT '0' AFTER `sef_route`;
 
+UPDATE `#__cck_core_fields` SET `script` = REPLACE( `script`, 'task=ajax&format=raw"', 'task=ajax&format=raw&"+Joomla.getOptions("csrf.token")+"=1"' ) WHERE `id` IN (58,59);
+
 UPDATE `#__cck_core_types` SET `parent_inherit`=1 WHERE `parent` != "";
 
 UPDATE `#__cck_core_fields` SET `script` = '', `attributes` = '' WHERE `id` = 27;
