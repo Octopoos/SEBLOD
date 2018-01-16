babc73d80b1db95a7b9eac1c8a9e6722a9d7f3ee
Sebastien Heraud <sebastien@heraud.me>
2018-01-16 13:09:00 +0100

    Quick change

--- components/com_cck/views/form/view.html.php
+++ components/com_cck/views/form/view.html.php
@@ -97,9 +97,7 @@ class CCKViewForm extends JViewLegacy
 		include JPATH_SITE.'/libraries/cck/base/form/form_inc.php';
 		$unique	=	$preconfig['formId'].'_'.@$type->name;
 		
-		if ( isset( $config['id'] ) ) {
-			JFactory::getSession()->set( 'cck_hash_'.$unique, JApplication::getHash( $id.'|'.$type->name.'|'.$config['id'].'|'.$config['copyfrom_id'] ) );
-		}
+		JFactory::getSession()->set( 'cck_hash_'.$unique, JApplication::getHash( (int)$id.'|'.@$type->name.'|'.@(int)$config['id'].'|'.@(int)$config['copyfrom_id'] ) );
 		JFactory::getSession()->set( 'cck_hash_'.$unique.'_context', json_encode( $config['context'] ) );
 
 		// Set
