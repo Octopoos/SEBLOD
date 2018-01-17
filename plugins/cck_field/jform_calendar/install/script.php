b709b6f26b81d88b1b7ca6fd3d94c02dc322d12a
Sebastien Heraud <sebastien@heraud.me>
2018-01-17 11:28:20 +0100

    Update copyrights

--- plugins/cck_field/jform_calendar/install/script.php
+++ plugins/cck_field/jform_calendar/install/script.php
@@ -1,8 +1,8 @@
 <?php
 /**
-* @version 			SEBLOD 3.x More
+* @version 			SEBLOD 3.x Core
 * @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
-* @url				http://www.seblod.com
+* @url				https://www.seblod.com
 * @editor			Octopoos - www.octopoos.com
 * @copyright		Copyright (C) 2009 - 2017 SEBLOD. All Rights Reserved.
 * @license 			GNU General Public License version 2 or later; see _LICENSE.php
