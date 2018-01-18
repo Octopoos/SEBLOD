982548330ca8944620c15bd0ba15a8bce5336c6a
Sebastien Heraud <sebastien@heraud.me>
2018-01-18 11:55:36 +0100

    Update SQL file

--- administrator/components/com_cck/install/upgrades/3.16.0.sql
+++ administrator/components/com_cck/install/upgrades/3.16.0.sql
@@ -3,6 +3,9 @@ ALTER TABLE `#__cck_core_types` ADD `parent_inherit` TINYINT(3) NOT NULL AFTER `
 
 ALTER TABLE `#__cck_core_searchs` ADD `sef_route_aliases` TINYINT(3) NOT NULL DEFAULT '0' AFTER `sef_route`;
 
+ALTER TABLE `#__cck_more_jobs` ADD `run_url` INT(11) NOT NULL DEFAULT '0' AFTER `run_as`;
+ALTER TABLE `#__cck_more_jobs` ADD `run_url_custom` VARCHAR(255) NOT NULL AFTER `run_url`;
+
 UPDATE `#__cck_more_ecommerce_zones` SET profile = CONCAT('{"conditions":[', profile, '],"do":"0"}') WHERE profile != "" AND profile NOT LIKE ('{"conditions":%');
 
 UPDATE `#__cck_core_fields` SET `script` = REPLACE( `script`, 'task=ajax&format=raw"', 'task=ajax&format=raw&"+Joomla.getOptions("csrf.token")+"=1"' ) WHERE `id` IN (58,59);
