26d0d42493d81ce91c6968af68406108b47f8d9b
Sebastien Heraud <sebastien@heraud.me>
2018-01-24 17:36:33 +0100

    Update SQL file

--- administrator/components/com_cck/install/upgrades/3.16.0.sql
+++ administrator/components/com_cck/install/upgrades/3.16.0.sql
@@ -6,6 +6,8 @@ ALTER TABLE `#__cck_core_searchs` ADD `sef_route_aliases` TINYINT(3) NOT NULL DE
 ALTER TABLE `#__cck_more_jobs` ADD `run_url` INT(11) NOT NULL DEFAULT '0' AFTER `run_as`;
 ALTER TABLE `#__cck_more_jobs` ADD `run_url_custom` VARCHAR(255) NOT NULL AFTER `run_url`;
 
+UPDATE `#__cck_core_fields` SET `defaultvalue` = '', `selectlabel` = 'Use Global', `options` = 'Email=1||Field=3' WHERE `id` = 124;
+
 UPDATE `#__cck_core_fields` SET `script` = REPLACE( `script`, 'task=ajax&format=raw"', 'task=ajax&format=raw&"+Joomla.getOptions("csrf.token")+"=1"' ) WHERE `id` IN (58,59);
 
 UPDATE `#__cck_core_types` SET `parent_inherit`=1 WHERE `parent` != "";
