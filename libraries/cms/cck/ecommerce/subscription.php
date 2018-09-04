3ce6fee7027062e2a33619300dfca1b5e0a084a9
Sebastien Heraud <sebastien@heraud.me>
2018-09-04 15:56:41 +0200

    Improve Ecommerce stuff

--- libraries/cms/cck/ecommerce/subscription.php
+++ libraries/cms/cck/ecommerce/subscription.php
@@ -10,24 +10,36 @@
 
 defined( '_JEXEC' ) or die;
 
+use Joomla\Utilities\ArrayHelper;
+
 // JCckEcommerceSubscription
 abstract class JCckEcommerceSubscription
 {
-	protected static function _getItems( $user_id = 0, $state = 1, $date_state = '' )
+	// _getItems
+	protected static function _getItems( $user_id, $state, $date_state, $limit = 0 )
 	{
-		$db		=	JFactory::getDbo();
-		$now	=	JFactory::getDate()->toSql();
-		$null	=	$db->getNullDate();
-		$query	=	$db->getQuery( true );
+		$db			=	JFactory::getDbo();
+		$grouped	=	false;
+		$now		=	JFactory::getDate()->toSql();
+		$null		=	$db->getNullDate();
+		$query		=	$db->getQuery( true );
 
 		$query->select( 'a.id, a.start_date, a.end_date, b.groups, a.user_id' )
 			  ->from( '`#__cck_more_ecommerce_subscriptions` AS a' )
-			  ->join( 'LEFT', '`#__cck_more_ecommerce_subscription_definitions` AS b ON b.name = a.type' );
+			  ->join( 'LEFT', '`#__cck_more_ecommerce_subscription_definitions` AS b ON b.name = a.type' )
+			  ->join( 'INNER', '`#__users` AS c ON c.id = a.user_id' )
+			  ;
 
 		$query->where( 'a.state = '.(int)$state );
 
-		if ( (int)$user_id > 0 ) {
-			$query->where( 'a.user_id = '.(int)$user_id );
+		if ( is_array( $user_id ) ) {
+			$grouped	=	true;
+			$user_id	=	ArrayHelper::toInteger( $user_id );
+			$query->where( 'a.user_id IN ('.implode( ',', $user_id ).')' );
+		} else {
+			if ( (int)$user_id > 0 ) {
+				$query->where( 'a.user_id = '.(int)$user_id );
+			}
 		}
 
 		if ( $date_state == 'expired' ) {
@@ -39,15 +51,41 @@ abstract class JCckEcommerceSubscription
 			// TODO
 		}
 
-		$db->setQuery( $query );
+		$query->order( 'a.id ASC' );
+		
+		$db->setQuery( $query, 0, $limit );
 
-		return $db->loadObjectList();
+		$list	=	$db->loadObjectList();
+
+		if ( $grouped ) {
+			$items	=	array();
+
+			foreach ( $list as $item ) {
+				if ( !isset( $items[$item->user_id] ) ) {
+					$items[$item->user_id]	=	array();
+				}
+
+				$items[$item->user_id][]	=	$item;
+			}
+
+			return $items;
+		} elseif ( $limit ) {
+			$ids	=	array();
+
+			foreach ( $list as $item ) {
+				$ids[]	=	$item->id;
+			}
+
+			JCckDatabase::execute( 'UPDATE `#__cck_more_ecommerce_subscriptions` SET state = -2 WHERE id IN ('.implode( ',', $ids ).')' );
+		}
+
+		return $list;
 	}
 
 	// expire
-	public static function expire()
+	public static function expire( $limit = 0 )
 	{
-		$expired_items		=	self::_getItems( 0, 1, 'expired' );
+		$expired_items		=	self::_getItems( 0, 1, 'expired', $limit );
 		$expired_triggers	=	array();
 
 		if ( count( $expired_items ) ) {
@@ -62,9 +100,6 @@ abstract class JCckEcommerceSubscription
 					$user_groups[$item->user_id]	=	array();
 				}
 
-				// Set as Expired
-				JCckDatabase::execute( 'UPDATE #__cck_more_ecommerce_subscriptions SET state = 0 WHERE id = '.(int)$item->id );
-
 				$expired_triggers[]				=	array(
 														'author'=>$item->user_id,
 														'pk'=>(int)$item->id
@@ -72,16 +107,18 @@ abstract class JCckEcommerceSubscription
 				$user_groups[$item->user_id]	=	array_merge( $user_groups[$item->user_id], explode( ',', $item->groups ) );
 			}
 
+			$user_ids			=	array_keys( $user_groups );
+			$all_active_items	=	self::_getItems( $user_ids, 1, 'active' );
+
 			foreach ( $user_groups as $user_id=>$groups ) {
 				$groups	=	array_flip( $groups );
 
 				// Unset from Groups
 				if ( count( $groups ) ) {
 					$valid_groups	=	array();
-					$active_items	=	self::_getItems( $user_id, 1, 'active' );
 
-					if ( count( $active_items ) ) {
-						foreach ( $active_items as $item ) {
+					if ( isset( $all_active_items[$user_id] ) ) {
+						foreach ( $all_active_items[$user_id] as $item ) {
 							$valid_groups	=	array_merge( $valid_groups, explode( ',', $item->groups ) );
 						}
 						$valid_groups	=	array_flip( $valid_groups );
@@ -98,10 +135,16 @@ abstract class JCckEcommerceSubscription
 
 			// Trigger "onCckSubsriptionExpiration" after suitable groups have been removed
 			if ( JCckToolbox::getConfig()->get( 'processing', 0 ) ) {
-				if ( count( $expired_triggers ) ) {
-					foreach ( $expired_triggers as $trigger_config ) {
-						JCckToolbox::process( 'onCckSubscriptionExpiration', $trigger_config );
-					}
+				foreach ( $expired_triggers as $trigger_config ) {
+					JCckToolbox::process( 'onCckSubscriptionExpiration', $trigger_config );
+
+					// Set as Expired
+					JCckDatabase::execute( 'UPDATE #__cck_more_ecommerce_subscriptions SET state = 0 WHERE id = '.(int)$trigger_config['pk'] );
+				}
+			} else {
+				foreach ( $expired_triggers as $trigger_config ) {
+					// Set as Expired
+					JCckDatabase::execute( 'UPDATE #__cck_more_ecommerce_subscriptions SET state = 0 WHERE id = '.(int)$trigger_config['pk'] );
 				}
 			}
 		}
