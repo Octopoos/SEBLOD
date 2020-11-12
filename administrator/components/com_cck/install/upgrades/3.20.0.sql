8a740e44121db6bbf64e628a02f5b011e88c7c27
Sebastien Heraud <sebastien@heraud.me>
2020-11-12 11:12:42 +0100

    Update SQL file

--- administrator/components/com_cck/install/upgrades/3.20.0.sql
+++ administrator/components/com_cck/install/upgrades/3.20.0.sql
@@ -1,2 +1,4 @@
 
 UPDATE `#__cck_core_fields` SET `selectlabel` = 'Auto' WHERE `name` = "core_orientation_vertical";
+
+UPDATE `#__cck_core_search_field` SET `link_options` = REPLACE( `link_options`, '"form_edition":"1"', '"form_edition":"0"' ) WHERE `link` = 'cck_form' AND `link_options` LIKE '{"form":%' AND `link_options` NOT LIKE '{"form":""%';
