6612aaef9a3d47d23e25f79d2081a49d3830f0b5
Sebastien Heraud <sebastien@heraud.me>
2021-08-22 22:05:27 +0200

    Remove code as "storage_params" column should NOT be there anymore

--- administrator/components/com_cck/tables/field.php
+++ administrator/components/com_cck/tables/field.php
@@ -58,9 +58,6 @@ class CCK_TableField extends JTable
 			if ( is_null( $this->storage_field2 ) ) {
 				$this->storage_field2	=	'';
 			}
-			if ( is_null( $this->storage_params ) ) {
-				$this->storage_params	=	'';
-			}
 			if ( is_null( $this->storages ) ) {
 				$this->storages	=	'';
 			}
