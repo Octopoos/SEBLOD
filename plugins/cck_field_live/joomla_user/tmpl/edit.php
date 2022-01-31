e2b565343f8f1dec25ad8b0cb87104f575e9390d
Sebastien Heraud <seb@heraud.me>
2022-01-31 23:02:02 +0100

    Refactor Live Construction/Params using Layout (2/2)

--- plugins/cck_field_live/joomla_user/tmpl/edit.php
+++ plugins/cck_field_live/joomla_user/tmpl/edit.php
@@ -12,32 +12,44 @@ defined( '_JEXEC' ) or die;
 
 JCckDev::initScript( 'live', $this->item );
 JCck::loadModalBox();
+
+// JS
+$js =	'jQuery(document).ready(function($) {
+			$("#excluded").isVisibleWhen("property","access,groups");
+			$(".value-picker").on("click", function() {
+				var field = $(this).attr("name");
+				var cur = "none";
+				var url = "index.php?option=com_cck&task=box.add&tmpl=component&file=administrator/components/com_cck/views/field/tmpl/selection.php&title=construct&name=joomla_user&type="+field+"&id=object_property";
+				$.colorbox({href:url, iframe:true, innerWidth:600, innerHeight:200, scrolling:false, overlayClose:false, fixed:true, className:"modal-small", onLoad: function(){ $("#cboxClose").remove();}});
+			});
+		});';
+
+// Set
+echo JCckDev::renderLayoutFile( 'cck'.JCck::v().'.construction.admin.edit', array(
+	'config'=>$config,
+	'form'=>array(
+		array(
+			'fields'=>array(
+				JCckDev::renderForm( 'core_dev_select', '', $config, array( 'defaultvalue'=>'', 'label'=>'User', 'selectlabel'=>'Current', 'options'=>'', 'storage_field'=>'content' ) ),
+				JCckDev::renderLayoutFile(
+					'cck'.JCck::v().'.form.field', array(
+						'label'=>JText::_( 'COM_CCK_PROPERTY' ),
+						'html'=>JCckDev::renderLayoutFile( 'cck'.JCck::v().'.construction.input', array(
+							'input'=>JCckDev::getForm( 'core_dev_text', '', $config, array( 'label'=>'Property', 'required'=>'required', 'storage_field'=>'property' ) ),
+							'button'=>'<button type="button" id="storage_field_pick_property" name="property" class="value-picker btn btn-secondary"><span class="icon-expand"></span></button>'
+						) )
+					)
+				),
+				JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Excluded Values', 'storage_field'=>'excluded' ) ),
+				JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Default Value', 'storage_field'=>'default_value' ) )
+			)
+		)
+	),
+	'html'=>'',
+	'item'=>$this->item,
+	'script'=>$js,
+	'type'=>'live'
+) );
+
+JCckDev::initScript( 'live', $this->item );
 ?>
-
-<div class="seblod">
-	<?php echo JCckDev::renderLegend( JText::_( 'COM_CCK_CONSTRUCTION' ), JText::_( 'PLG_CCK_FIELD_LIVE_'.$this->item->name.'_DESC' ) ); ?>
-    <ul class="adminformlist adminformlist-2cols">
-        <?php
-		echo JCckDev::renderForm( 'core_dev_select', '', $config, array( 'defaultvalue'=>'', 'label'=>'User', 'selectlabel'=>'Current', 'options'=>'', 'storage_field'=>'content' ) );
-		echo '<li><label>'.JText::_( 'COM_CCK_PROPERTY' ).'<span class="star"> *</span></label>'
-		 .	 JCckDev::getForm( 'core_dev_text', '', $config, array( 'label'=>'Property', 'required'=>'required', 'storage_field'=>'property' ) )
-		 .	 '<span id="storage_field_pick_property" name="property" class="value-picker"><span class="icon-menu-2"></span></span>';
-		 ;
-		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Excluded Values', 'storage_field'=>'excluded' ) );
-		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Default Value', 'storage_field'=>'default_value' ) );
-		?>
-    </ul>
-</div>
-
-<script type="text/javascript">
-jQuery(document).ready(function($) {
-	$('#excluded').isVisibleWhen('property','access,groups');
-
-	$("span.value-picker").on("click", function() {
-        var field = $(this).attr("name");
-        var cur = "none";
-        var url = "index.php?option=com_cck&task=box.add&tmpl=component&file=administrator/components/com_cck/views/field/tmpl/selection.php&title=construct&name=joomla_user&type="+field+"&id=object_property";
-        $.colorbox({href:url, iframe:true, innerWidth:300, innerHeight:200, scrolling:false, overlayClose:false, fixed:true, onLoad: function(){ $('#cboxClose').remove();}});
-    });
-});
-</script>
--- plugins/cck_field_live/url_variable/tmpl/edit.php
+++ plugins/cck_field_live/url_variable/tmpl/edit.php
@@ -9,31 +9,35 @@
 **/
 
 defined( '_JEXEC' ) or die;
-?>
 
-<div class="seblod">
-	<?php echo JCckDev::renderLegend( JText::_( 'COM_CCK_CONSTRUCTION' ), JText::_( 'PLG_CCK_FIELD_LIVE_'.$this->item->name.'_DESC' ) ); ?>
-	<ul class="adminformlist adminformlist-2cols">
-		<?php
-		echo JCckDev::renderForm( 'core_variable_type', '', $config );
-		echo JCckDev::renderForm( 'core_bool', '', $config, array( 'label'=>'Multiple', 'storage_field'=>'multiple' ) );
-		echo JCckDev::renderForm( 'core_dev_texts', '', $config, array( 'label'=>'Variables', 'storage_field'=>'variables' ) );
-		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Variable', 'storage_field'=>'variable' ) );
-		echo JCckDev::renderForm( 'core_bool', '', $config, array( 'label'=>'Ignore NULL', 'storage_field'=>'ignore_null' ) );
-		echo JCckDev::renderForm( 'core_bool', '', $config, array( 'label'=>'Return', 'options'=>'First=first||Last=last', 'storage_field'=>'return' ) );
-		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Default Value', 'storage_field'=>'default_value' ) );
-		echo JCckDev::renderForm( 'core_dev_select', '', $config, array( 'label'=>'Encryption', 'selectlabel'=>'None', 'options'=>'Base64=base64', 'bool8'=>0, 'storage_field'=>'crypt' ) );
-		?>
-	</ul>
-</div>
+// JS
+$js =	'jQuery(document).ready(function($) {
+			$("#sortable_core_dev_texts, #return").isVisibleWhen("multiple","1");
+			$("#variable,#default_value,#crypt").isVisibleWhen("multiple","0");
+		});';
 
-<?php
+// Set
+echo JCckDev::renderLayoutFile( 'cck'.JCck::v().'.construction.admin.edit', array(
+	'config'=>$config,
+	'form'=>array(
+		array(
+			'fields'=>array(
+				JCckDev::renderForm( 'core_variable_type', '', $config ),
+				JCckDev::renderForm( 'core_bool', '', $config, array( 'label'=>'Multiple', 'storage_field'=>'multiple' ) ),
+				JCckDev::renderForm( 'core_dev_texts', '', $config, array( 'label'=>'Variables', 'storage_field'=>'variables' ) ),
+				JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Variable', 'storage_field'=>'variable' ) ),
+				JCckDev::renderForm( 'core_bool', '', $config, array( 'label'=>'Ignore NULL', 'storage_field'=>'ignore_null' ) ),
+				JCckDev::renderForm( 'core_bool', '', $config, array( 'label'=>'Return', 'options'=>'First=first||Last=last', 'storage_field'=>'return' ) ),
+				JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Default Value', 'storage_field'=>'default_value' ) ),
+				JCckDev::renderForm( 'core_dev_select', '', $config, array( 'label'=>'Encryption', 'selectlabel'=>'None', 'options'=>'Base64=base64', 'bool8'=>0, 'storage_field'=>'crypt' ) )
+			)
+		)
+	),
+	'html'=>'',
+	'item'=>$this->item,
+	'script'=>$js,
+	'type'=>'live'
+) );
+
 JCckDev::initScript( 'live', $this->item );
 ?>
-
-<script type="text/javascript">
-jQuery(document).ready(function($) {
-	$('#sortable_core_dev_texts, #return').isVisibleWhen('multiple','1');
-	$('#variable,#default_value,#crypt').isVisibleWhen('multiple','0');
-});
-</script>
