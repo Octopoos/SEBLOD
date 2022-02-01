018ca8cbece09d761f641c6dbc5f03e7fd7a6872
Sebastien Heraud <seb@heraud.me>
2022-02-01 19:13:34 +0100

    Update SQL files (1/n)

--- administrator/components/com_cck/install/sql/jobs.sql
+++ administrator/components/com_cck/install/sql/jobs.sql
@@ -12,8 +12,8 @@ CREATE TABLE IF NOT EXISTS `#__cck_more_jobs` (
   `run_as` int(10) unsigned NOT NULL DEFAULT '0',
   `run_url` int(11) NOT NULL DEFAULT '0',
   `run_url_custom` varchar(255) NOT NULL,
-  `checked_out` int(10) unsigned NOT NULL DEFAULT '0',
-  `checked_out_time` datetime NOT NULL,
+  `checked_out` int(10) UNSIGNED,
+  `checked_out_time` datetime NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=500 ;
