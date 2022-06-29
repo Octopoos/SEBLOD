de2e72537f712376600d04988ddaf74f0dcada49
Sebastien Heraud <seb@heraud.me>
2022-06-29 11:41:12 +0200

    Refactor Link Construction/Params using Layout (2/2)

--- plugins/cck_field_link/cck_list/tmpl/edit.php
+++ plugins/cck_field_link/cck_list/tmpl/edit.php
@@ -11,7 +11,9 @@
 defined( '_JEXEC' ) or die;
 
 JCckDev::initScript( 'link', $this->item );
+
 $hide	=	$this->item->alt ? 'hide' : '';
+
 if ( $this->item->alt ) {
 	$hide		=	'hide';
 	$required	=	'';
@@ -19,40 +21,57 @@ if ( $this->item->alt ) {
 	$hide		=	'';
 	$required	=	'required';
 }
+
+// JS
+$js =	'jQuery(document).ready(function($) {
+			$("#search_fieldname").isVisibleWhen("search_field","1");
+			$("#target_params").isVisibleWhen("target","modal");
+			$("#title_custom").isVisibleWhen("title","2,3",false);
+		});';
+
+// Set
+echo JCckDev::renderLayoutFile( 'cck'.JCck::v().'.construction.admin.edit', array(
+	'config'=>$config,
+	'form'=>array(
+		array(
+			'fields'=>array(
+				JCckDev::renderForm( 'core_list', '', $config, array(), array(), $hide ),
+				JCckDev::renderForm( 'core_menuitem', '', $config, array( 'required'=>$required ) ),
+				JCckDev::renderForm( 'core_dev_select', '', $config, array( 'defaultvalue'=>0, 'label'=>'Field', 'options'=>'None=-1||Field=optgroup||Inherited=0||Custom=1', 'selectlabel'=>'', 'storage_field'=>'search_field' ), array(), $hide ),
+				JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Field name', 'required'=>'required', 'storage_field'=>'search_fieldname' ), array(), $hide )
+			)
+		),
+		array(
+			'fields'=>array(
+				JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Class', 'size'=>24, 'storage_field'=>'class' ) ),
+				JCckDev::renderForm( 'core_options_target', '', $config, array( 'defaultvalue'=>'', 'selectlabel'=>'Inherited', 'options'=>'Target Blank=_blank||Target Self=_self||Target Parent=_parent||Target Top=_top||Advanced=optgroup||Modal Box=modal', 'storage_field'=>'target' ) ),
+				JCckDev::renderForm( 'core_dev_textarea', '', $config, array( 'label'=>'Parameters', 'cols'=>80, 'rows'=>1, 'storage_field'=>'target_params' ), array(), 'w100' ),
+				JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Rel', 'size'=>32, 'storage_field'=>'rel' ) ),
+				JCckDev::renderLayoutFile(
+					'cck'.JCck::v().'.form.field', array(
+						'label'=>JText::_( 'COM_CCK_TITLE' ),
+						'html'=>JCckDev::renderLayoutFile( 'cck'.JCck::v().'.construction.grid', array(
+							'grid'=>'|50%',
+							'html'=>array(
+								JCckDev::getForm( 'core_dev_select', '', $config, array( 'selectlabel'=>'None', 'options'=>'Custom Text=2||Translated Text=3', 'storage_field'=>'title' ) ),
+								JCckDev::getForm( 'core_dev_text', '', $config, array( 'label'=>'Title', 'size'=>16, 'css'=>'input-medium', 'storage_field'=>'title_custom' ) )
+							)
+						) )
+					)
+				),
+				JCckDev::renderBlank(),
+				JCckDev::renderForm( 'core_tmpl', '', $config ),
+				JCckDev::renderForm( 'core_dev_textarea', '', $config, array( 'label'=>'Custom variables', 'cols'=>92, 'rows'=>1, 'storage_field'=>'custom' ), array(), 'w100' ),
+				JCckDev::renderForm( 'core_dev_select', '', $config, array( 'label'=>'Behavior', 'type'=>'radio', 'selectlabel'=>'', 'defaultvalue'=>'1', 'options'=>'Prepare=0||Apply=1', 'css'=>'btn-group', 'storage_field'=>'state' ) ),
+			),
+			'legend'=>JText::_( 'COM_CCK_OPTIONS' )
+		)
+	),
+	'html'=>'',
+	'item'=>$this->item,
+	'script'=>$js,
+	'type'=>'link'
+) );
+
+JCckDev::initScript( 'link', $this->item );
 ?>
-
-<div class="seblod">
-	<?php echo JCckDev::renderLegend( JText::_( 'COM_CCK_CONSTRUCTION' ), JText::_( 'PLG_CCK_FIELD_LINK_'.$this->item->name.'_DESC' ) ); ?>
-    <ul class="adminformlist adminformlist-2cols">
-        <?php
-		echo JCckDev::renderForm( 'core_list', '', $config, array(), array(), $hide );
-		echo JCckDev::renderForm( 'core_menuitem', '', $config, array( 'required'=>$required ) );
-		echo JCckDev::renderForm( 'core_dev_select', '', $config, array( 'defaultvalue'=>0, 'label'=>'Field', 'options'=>'None=-1||Field=optgroup||Inherited=0||Custom=1',
-																		 'selectlabel'=>'', 'storage_field'=>'search_field' ), array(), $hide );
-		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Field name', 'required'=>'required', 'storage_field'=>'search_fieldname' ), array(), $hide );
-		
-		echo JCckDev::renderSpacer( JText::_( 'COM_CCK_CONSTRUCTION' ) . '<span class="mini">('.JText::_( 'COM_CCK_GENERIC' ).')</span>' );
-		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Class', 'size'=>24, 'storage_field'=>'class' ) );
-		echo JCckDev::renderForm( 'core_options_target', '', $config, array( 'defaultvalue'=>'', 'selectlabel'=>'Inherited', 'options'=>'Target Blank=_blank||Target Self=_self||Target Parent=_parent||Target Top=_top||Advanced=optgroup||Modal Box=modal', 'storage_field'=>'target' ) );
-		echo JCckDev::renderForm( 'core_dev_textarea', '', $config, array( 'label'=>'Parameters', 'cols'=>80, 'rows'=>1, 'storage_field'=>'target_params' ), array(), 'w100' );
-		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Rel', 'size'=>32, 'storage_field'=>'rel' ) );
-		echo '<li><label>'.JText::_( 'COM_CCK_TITLE' ).'</label>'
-			. JCckDev::getForm( 'core_dev_select', '', $config, array( 'selectlabel'=>'None', 'options'=>'Custom Text=2||Translated Text=3', 'storage_field'=>'title' ) )
-			. JCckDev::getForm( 'core_dev_text', '', $config, array( 'label'=>'Title', 'size'=>16, 'css'=>'input-medium', 'storage_field'=>'title_custom' ) )
-			. '</li>';
-		echo JCckDev::renderBlank();
-		echo JCckDev::renderForm( 'core_tmpl', '', $config );
-		echo JCckDev::renderForm( 'core_dev_textarea', '', $config, array( 'label'=>'Custom variables', 'cols'=>92, 'rows'=>1, 'storage_field'=>'custom' ), array(), 'w100' );
-		echo JCckDev::renderForm( 'core_dev_select', '', $config, array( 'label'=>'Behavior', 'selectlabel'=>'', 'defaultvalue'=>'1', 'options'=>'Apply=1||Prepare=0', 'storage_field'=>'state' ) );
-		echo JCckDev::renderBlank();
-        ?>
-    </ul>
-</div>
-
-<script type="text/javascript">
-jQuery(document).ready(function($) {
-	$('#search_fieldname').isVisibleWhen('search_field','1');
-	$('#target_params').isVisibleWhen('target','modal');
-	$('#title_custom').isVisibleWhen('title','2,3',false);
-});
-</script>
--- plugins/cck_field_link/content_delete/tmpl/edit.php
+++ plugins/cck_field_link/content_delete/tmpl/edit.php
@@ -11,38 +11,57 @@
 defined( '_JEXEC' ) or die;
 
 JCckDev::initScript( 'link', $this->item );
+
+// JS
+$js =	'jQuery(document).ready(function($) {
+			$("#title_custom").isVisibleWhen("title","2,3",false);
+			$("#redirection_url,#blank_li").isVisibleWhen("redirection","url");
+			$("#redirection_custom,#blank_li2").isVisibleWhen("redirection","");
+		});';
+
+// Set
+echo JCckDev::renderLayoutFile( 'cck'.JCck::v().'.construction.admin.edit', array(
+	'config'=>$config,
+	'form'=>array(
+		array(
+			'fields'=>array(
+				JCckDev::renderForm( 'core_dev_bool', '', $config, array( 'defaultvalue'=>1, 'label'=>'Confirm', 'storage_field'=>'confirm' ) ),
+				JCckDev::renderForm( 'core_dev_select', '', $config, array( 'label'=>'Redirection', 'selectlabel'=>'Auto', 'options'=>'Url=url', 'storage_field'=>'redirection' ) ),
+				JCckDev::renderBlank( '<input type="hidden" id="blank_li" value="" />' ),
+				JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Url', 'storage_field'=>'redirection_url' ) ),
+				JCckDev::renderBlank( '<input type="hidden" id="blank_li2" value="" />' ),
+				JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Redirection Custom Variables', 'storage_field'=>'redirection_custom' ) )
+			)
+		),
+		array(
+			'fields'=>array(
+				JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Class', 'size'=>24, 'storage_field'=>'class' ) ),
+				JCckDev::renderLayoutFile(
+						'cck'.JCck::v().'.form.field', array(
+							'label'=>JText::_( 'COM_CCK_TITLE' ),
+							'html'=>JCckDev::renderLayoutFile( 'cck'.JCck::v().'.construction.grid', array(
+								'grid'=>'|50%',
+								'html'=>array(
+									JCckDev::getForm( 'core_dev_select', '', $config, array( 'selectlabel'=>'None', 'options'=>'Custom Text=2||Translated Text=3', 'storage_field'=>'title' ) ),
+									JCckDev::getForm( 'core_dev_text', '', $config, array( 'label'=>'Title', 'size'=>16, 'css'=>'input-medium', 'storage_field'=>'title_custom' ) )
+								)
+							) )
+						)
+					),
+				JCckDev::renderForm( 'core_dev_select', '', $config, array( 'label'=>'Behavior', 'type'=>'radio', 'selectlabel'=>'', 'defaultvalue'=>'1', 'options'=>'Prepare=0||Apply=1', 'css'=>'btn-group', 'storage_field'=>'state' ) )
+			),
+			'legend'=>JText::_( 'COM_CCK_OPTIONS' )
+		),
+		array(
+			'fields'=>array(
+				JCckDev::renderForm( 'core_dev_select', '', $config, array( 'defaultvalue'=>'0', 'label'=>'Show Value', 'type'=>'radio', 'selectlabel'=>'', 'options'=>'Hide=0||Show=1', 'css'=>'btn-group', 'storage_field'=>'no_access' ) )
+			),
+			'legend'=>JText::_( 'COM_CCK_CONFIG_NO_ACCESS' )
+		)
+	),
+	'html'=>'',
+	'item'=>$this->item,
+	'script'=>$js,
+	'type'=>'link'
+) );
 ?>
-
-<div class="seblod">
-	<?php echo JCckDev::renderLegend( JText::_( 'COM_CCK_CONSTRUCTION' ), JText::_( 'PLG_CCK_FIELD_LINK_'.$this->item->name.'_DESC' ) ); ?>
-    <ul class="adminformlist adminformlist-2cols">
-        <?php
-		echo JCckDev::renderForm( 'core_dev_bool', '', $config, array( 'defaultvalue'=>1, 'label'=>'Confirm', 'storage_field'=>'confirm' ) );
-		echo JCckDev::renderForm( 'core_dev_select', '', $config, array( 'label'=>'Redirection', 'selectlabel'=>'Auto', 'options'=>'Url=url', 'storage_field'=>'redirection' ) );
-		echo JCckDev::renderBlank( '<input type="hidden" id="blank_li" value="" />' );
-		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Url', 'storage_field'=>'redirection_url' ) );
-		echo JCckDev::renderBlank( '<input type="hidden" id="blank_li2" value="" />' );
-		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Redirection Custom Variables', 'storage_field'=>'redirection_custom' ) );
-
-		echo JCckDev::renderSpacer( JText::_( 'COM_CCK_CONSTRUCTION' ) . '<span class="mini">('.JText::_( 'COM_CCK_GENERIC' ).')</span>' );
-		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Class', 'size'=>24, 'storage_field'=>'class' ) );
-		echo '<li><label>'.JText::_( 'COM_CCK_TITLE' ).'</label>'
-			. JCckDev::getForm( 'core_dev_select', '', $config, array( 'selectlabel'=>'None', 'options'=>'Custom Text=2||Translated Text=3', 'storage_field'=>'title' ) )
-			. JCckDev::getForm( 'core_dev_text', '', $config, array( 'label'=>'Title', 'size'=>16, 'css'=>'input-medium', 'storage_field'=>'title_custom' ) )
-			. '</li>';
-		echo JCckDev::renderForm( 'core_dev_select', '', $config, array( 'label'=>'Behavior', 'selectlabel'=>'', 'defaultvalue'=>'1', 'options'=>'Apply=1||Prepare=0', 'storage_field'=>'state' ) );
-		echo JCckDev::renderBlank();
-
-		echo JCckDev::renderSpacer( JText::_( 'COM_CCK_CONFIG_NO_ACCESS' ) );
-		echo JCckDev::renderForm( 'core_dev_select', '', $config, array( 'defaultvalue'=>'0', 'label'=>'Show Value', 'selectlabel'=>'', 'options'=>'Hide=0||Show=1', 'storage_field'=>'no_access' ) );
-		?>
-    </ul>
-</div>
-
-<script type="text/javascript">
-jQuery(document).ready(function($) {
-	$('#title_custom').isVisibleWhen('title','2,3',false);
-	$('#redirection_url,#blank_li').isVisibleWhen('redirection','url');
-	$('#redirection_custom,#blank_li2').isVisibleWhen('redirection','');
-});
-</script>
