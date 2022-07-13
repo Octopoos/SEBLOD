f7658b7cb483832674fd0318e37722dd2a7f52de
Sebastien Heraud <seb@heraud.me>
2022-07-13 11:58:46 +0200

    Update Layouts

--- layouts/cck/form/field.php
+++ layouts/cck/form/field.php
@@ -1,7 +1,9 @@
 <?php
 defined( '_JEXEC' ) or die;
+
+$class	=	isset( $displayData['class'] ) ? ' '.$displayData['class'] : '';
 ?>
-<div class="control-group">
+<div class="control-group<?php echo $class; ?>">
 	<div class="control-label"><label><?php echo $displayData['label']; ?></label></div>
 	<div class="controls"><?php echo $displayData['html']; ?></div>
 </div>
