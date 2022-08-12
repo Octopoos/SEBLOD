20f09cd01ea69317842b53a73261edb6e49039d9
Sebastien Heraud <seb@heraud.me>
2022-08-12 16:56:37 +0200

    Push Changes again (!)

--- libraries/cms/cck/device.php
+++ libraries/cms/cck/device.php
@@ -14,21 +14,91 @@ use Joomla\CMS\Environment\Browser;
 
 JLoader::register( 'Mobile_Detect', JPATH_PLATFORM.'/cck/misc/Mobile_Detect.php' );
 
+// JCckBrowser
+class JCckBrowser extends Browser
+{
+	protected static $instances = array();
+
+	// getInstance
+	public static function getInstance( $userAgent = null, $accept = null )
+	{
+		$signature	=	serialize( array( $userAgent, $accept ) );
+
+		if ( empty( self::$instances[$signature] ) ) {
+			self::$instances[$signature]	=	new JCckBrowser( $userAgent, $accept );
+		}
+
+		return self::$instances[$signature];
+	}
+
+	// setVersions
+	public function setVersions( $version )
+	{
+		if ( strpos( $version[1], '.' ) !== false) {
+			$parts	=	explode( '.', $version[1] );
+
+			$this->majorVersion	=	$parts[0];
+			$this->minorVersion	=	$parts[1];
+		} else {
+			$this->majorVersion	=	$version[1];
+			$this->minorVersion	=	0;
+		}
+	}
+}
+
 // JCckDevice
 class JCckDevice extends Mobile_Detect
 {
-	protected $_platform	=	null;
+	protected static $_instance;
+
+	protected $_browser	=	null;
+
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Construct
+
+	// getInstance
+	public static function getInstance()
+	{
+		if ( !self::$_instance ) {
+			self::$_instance			=	new JCckDevice;
+			self::$_instance->_browser	=	JCckBrowser::getInstance();
+
+			$version	=	array();
+
+			if ( self::$_instance->_browser->getBrowser() == 'safari'
+			  && preg_match( '|QQBrowserLite\/([0-9.]+)|', self::$_instance->_browser->getAgentString(), $version ) ) {
+				self::$_instance->_browser->setBrowser( 'qqbrowserlite' );
+				self::$_instance->_browser->setVersions( $version );
+			} elseif ( preg_match( '|QQBrowser\/([0-9.]+)|', self::$_instance->_browser->getAgentString(), $version ) ) {
+				self::$_instance->_browser->setBrowser( 'qqbrowser' );
+				self::$_instance->_browser->setVersions( $version );
+			}
+		}
+
+		return self::$_instance;
+	}
+
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Get
+
+	// getBrowserName
+	public function getBrowserName()
+	{
+		return $this->_browser->getBrowser();
+	}
+
+	// getBrowserVersion
+	public function getBrowserVersion()
+	{
+		return $this->_browser->getVersion();
+	}
 
 	// isMacOs
 	public function isMacOs()
 	{
 		if ( $this->isMobile() ) {
 			return false;
-		} elseif ( is_null( $this->_platform ) ) {
-			$this->_platform	=	Browser::getInstance();
 		}
 
-		return $this->_platform->getPlatform() == 'mac' ? true : false; 
+		return $this->_browser->getPlatform() == 'mac' ? true : false; 
 	}
 }
 ?>
