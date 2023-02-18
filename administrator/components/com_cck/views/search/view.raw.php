6c61d018d052bf2da31a9a61b05c423be93eebb2
Sebastien Heraud <seb@heraud.me>
2023-02-18 19:16:19 +0700

    Additional fixes on PHP 8.x

--- administrator/components/com_cck/views/search/view.raw.php
+++ administrator/components/com_cck/views/search/view.raw.php
@@ -214,9 +214,9 @@ class CCKViewSearch extends JViewLegacy
 	public function setPosition( $name, $title = '' )
 	{
 		$title	=	( !empty( $title ) ) ? $title : $name;
-		$legend	=	'<input class="thin blue" type="text" name="ffp[pos-'.$name.'][legend]" value="'.htmlspecialchars( @$this->positions[$name]->legend ).'" size="22" />';
+		$legend	=	'<input class="thin blue" type="text" name="ffp[pos-'.$name.'][legend]" value="'.( isset( $this->positions[$name]->legend ) ? htmlspecialchars( $this->positions[$name]->legend ) : '' ).'" size="22" />';
 		$variat	=	Jhtml::_( 'select.genericlist', $this->variations, 'ffp[pos-'.$name.'][variation]', 'size="1" class="thin blue c_var_ck"', 'value', 'text', @$this->positions[$name]->variation, 'pos-'.$name.'_variation' );
-		$variat	.=	'<input type="hidden" id="pos-'.$name.'_variation_options" name="ffp[pos-'.$name.'][variation_options]" value="'.htmlspecialchars( @$this->positions[$name]->variation_options ).'" />';
+		$variat	.=	'<input type="hidden" id="pos-'.$name.'_variation_options" name="ffp[pos-'.$name.'][variation_options]" value="'.( isset( $this->positions[$name]->variation_options ) ? htmlspecialchars( $this->positions[$name]->variation_options ) : '' ).'" />';
 		$width	=	'<input class="thin blue" type="text" name="ffp[pos-'.$name.'][width]" value="'.@$this->positions[$name]->width.'" size="8" style="text-align:center;" />&nbsp;×&nbsp;';
 		$height	=	'<input class="thin blue" type="text" name="ffp[pos-'.$name.'][height]" value="'.@$this->positions[$name]->height.'" size="8" style="text-align:center;" />';
 		$css	=	'<input class="thin blue" type="text" name="ffp[pos-'.$name.'][css]" value="'.@$this->positions[$name]->css.'" size="22" />';
