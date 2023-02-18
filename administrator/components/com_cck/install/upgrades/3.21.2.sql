04acf93249bb3091797d3b74a7ec650c8f2f0fad
Sebastien Heraud <seb@heraud.me>
2023-02-18 21:01:37 +0700

    Update 3.21.2 SQL file

--- administrator/components/com_cck/install/upgrades/3.21.2.sql
+++ administrator/components/com_cck/install/upgrades/3.21.2.sql
@@ -1,3 +1,3 @@
 
-UPDATE `#__cck_core_fields` SET `options2` = REPLACE( `options2`, '"task_id_process":', '"task_id":' ) WHERE `type` = "button_submit";
-UPDATE `#__cck_core_fields` SET `options2` = REPLACE( `options2`, '"task_id_export":', '"task_id":' ) WHERE `type` = "button_submit";
+UPDATE `#__cck_core_fields` SET `options2` = REPLACE( `options2`, '"task_id_process":', '"task_id":' ) WHERE `type` = "button_submit" AND `options2` LIKE '%"task":"process%';
+UPDATE `#__cck_core_fields` SET `options2` = REPLACE( `options2`, '"task_id_export":', '"task_id":' ) WHERE `type` = "button_submit" AND `options2` LIKE '%"task":"export%';
