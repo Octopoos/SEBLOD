095054f4be2c68ae25422a85a6da3828bd3968bd
Sebastien Heraud <seb@heraud.me>
2023-02-28 11:43:16 +0700

    Update core_access again in 4.0.2 (rp8)

---
+++ administrator/components/com_cck/install/upgrades/4.0.2.sql
@@ -0,0 +1,2 @@
+
+UPDATE `#__cck_core_fields` SET `type` = 'select_dynamic', `options2` = '{"query":"","table":"#__viewlevels","name":"title","where":"","value":"id","orderby":"ordering","orderby_direction":"ASC","limit":"","language_detection":"joomla","language_codes":"EN,GB,US,FR","language_default":"EN","attr1":"","attr2":"","attr3":"","attr4":"","attr5":"","attr6":""}' WHERE `name` = 'core_access';
