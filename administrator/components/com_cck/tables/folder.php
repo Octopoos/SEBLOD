4d41e37d1dab67d35463d41fa007c61cf7c2b267
Sebastien Heraud <seb@heraud.me>
2023-03-01 11:35:16 +0700

    Bring back change

--- administrator/components/com_cck/tables/folder.php
+++ administrator/components/com_cck/tables/folder.php
@@ -26,13 +26,22 @@ class CCK_TableFolder extends JTable
 		if ( empty( $this->title ) ) {
 			return false;
 		}
-		if( empty( $this->name ) ) {
+		if ( empty( $this->name ) ) {
 			$this->name	=	$this->title;
 			$this->name =	JCckDev::toSafeSTRING( $this->name );
 			if( trim( str_replace( '_', '', $this->name ) ) == '' ) {
 				$datenow	=	JFactory::getDate();
 				$this->name =	$datenow->format( 'Y_m_d_H_i_s' );
 			}
+		}
+		if ( empty( $this->path ) ) {
+			$this->path	=	'';
+		}
+		if ( empty( $this->icon_path ) ) {
+			$this->icon_path	=	'';
+		}
+		if ( empty( $this->app ) ) {
+			$this->app	=	'';
 		}
 		
 		$prefix	=	JCck::getConfig_Param( 'development_prefix', '' );
