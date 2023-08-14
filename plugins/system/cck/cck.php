cc1ae5b4cc78f937465e71bf334c4118c20d4c0f
Sebastien Heraud <seb@heraud.me>
2023-08-14 12:03:26 +0200

    Quick Fix

--- plugins/system/cck/cck.php
+++ plugins/system/cck/cck.php
@@ -191,8 +191,10 @@ class plgSystemCCK extends JPlugin
 
 		if ( $app->isClient( 'site' ) ) {
 			if ( $option == 'com_content' && $view == 'category' ) {
-				/* TODO#SEBLOD4: We may need another condition but it can't be with $app->getMenu()->getActive()->query['option'] */
-				$app->setUserState( 'com_content.category.list.'.(int)$id.':'.(int)$itemId.'.limit', -1 );
+				if ( JCck::is( '5' ) ) {
+					/* TODO#SEBLOD4: We may need another condition but it can't be with $app->getMenu()->getActive()->query['option'] */
+					$app->setUserState( 'com_content.category.list.'.(int)$id.':'.(int)$itemId.'.limit', -1 );
+				}
 			}
 		}
 
