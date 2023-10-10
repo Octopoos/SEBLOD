c49b9f2d21ab5f3341e5e21f293a281443ad6787
Sebastien Heraud <seb@heraud.me>
2023-10-10 18:21:40 +0200

    Prepare SEBLOD 3.27.0 release

--- administrator/components/com_cck/_VERSION.php
+++ administrator/components/com_cck/_VERSION.php
@@ -13,11 +13,11 @@ defined( '_JEXEC' ) or die;
 // JCckVersion
 final class JCckVersion extends JCckVersionObject
 {
-	public $RELEASE = '3.26';
+	public $RELEASE = '3.27';
 	
-	public $DEV_LEVEL = '1';
+	public $DEV_LEVEL = '0';
 
-	public $DEV_STATUS = '';
+	public $DEV_STATUS = 'Beta';
 
 	public $API_VERSION = array( 'v3'=>'3.13.0' );
 	
