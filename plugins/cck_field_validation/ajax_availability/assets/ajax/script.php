7ea0acbc602c6d98e9d5899ea06b5493e4dc5cbf
Sebastien Heraud <seb@heraud.me>
2024-01-24 11:33:38 +0100

    Allow empty value (AJAX Availability)

--- plugins/cck_field_validation/ajax_availability/assets/ajax/script.php
+++ plugins/cck_field_validation/ajax_availability/assets/ajax/script.php
@@ -22,34 +22,37 @@ $key		=	$app->input->get( 'avKey', '' );
 $where		=	$app->input->getString( 'avWhere', '' );
 $table		=	$app->input->get( 'avTable', '' );
 
-// Process
-if ( $where ) {
-	$fields	=	JCckDatabase::loadObjectList( 'SELECT name, storage, storage_table, storage_field FROM #__cck_core_fields WHERE name IN ("'.str_replace( ',', '","', $where ).'")', 'name' );
-	$where	=	explode( ',', $where );
-	foreach ( $where as $w ) {
-		if ( isset( $fields[$w] ) && $fields[$w]->storage == 'standard' && $fields[$w]->storage_table == '#__'.$table ) {
-			$v		=	$app->input->get( $w );
-			if ( $v != '' ) {
-				$and	.=	' '.$fields[$w]->storage_field.'="'.JCckDatabase::escape( $v ).'"';
+if ( $value != '' ) {
+	// Process
+	if ( $where ) {
+		$fields	=	JCckDatabase::loadObjectList( 'SELECT name, storage, storage_table, storage_field FROM #__cck_core_fields WHERE name IN ("'.str_replace( ',', '","', $where ).'")', 'name' );
+		$where	=	explode( ',', $where );
+		foreach ( $where as $w ) {
+			if ( isset( $fields[$w] ) && $fields[$w]->storage == 'standard' && $fields[$w]->storage_table == '#__'.$table ) {
+				$v		=	$app->input->get( $w );
+				if ( $v != '' ) {
+					$and	.=	' '.$fields[$w]->storage_field.'="'.JCckDatabase::escape( $v ).'"';
+				}
 			}
 		}
+		$and	=	( $and ) ? ' AND'.$and : '';
 	}
-	$and	=	( $and ) ? ' AND'.$and : '';
-}
-if ( $key ) {
-	$pk		=	$app->input->getInt( 'avPk', 0 );
-	$pv		=	$app->input->getString( 'avPv', '' );
-	$pv		=	str_replace( array( '%26lt;', '%26gt;', '%27' ), array( '<', '>', "'" ), $pv );
-	$count	=	(int)JCckDatabase::loadResult( 'SELECT '.JCckDatabase::quoteName( $key ).' FROM '.JCckDatabase::quoteName( '#__'.$table ).' WHERE '.JCckDatabase::quoteName( $column ).' = "'.JCckDatabase::escape( $value ).'"'.$and );
-	$res[1]	=	( $count > 0 && $count != $pk ) ? false : true;
+	if ( $key ) {
+		$pk		=	$app->input->getInt( 'avPk', 0 );
+		$pv		=	$app->input->getString( 'avPv', '' );
+		$pv		=	str_replace( array( '%26lt;', '%26gt;', '%27' ), array( '<', '>', "'" ), $pv );
+		$count	=	(int)JCckDatabase::loadResult( 'SELECT '.JCckDatabase::quoteName( $key ).' FROM '.JCckDatabase::quoteName( '#__'.$table ).' WHERE '.JCckDatabase::quoteName( $column ).' = "'.JCckDatabase::escape( $value ).'"'.$and );
+		$res[1]	=	( $count > 0 && $count != $pk ) ? false : true;
+	} else {
+		$count	=	(int)JCckDatabase::loadResult( 'SELECT COUNT('.$column.') FROM '.JCckDatabase::quoteName( '#__'.$table ).' WHERE '.JCckDatabase::quoteName( $column ).' = "'.JCckDatabase::escape( $value ).'"'.$and );
+		$res[1]	=	( $count > 0 ) ? false : true;
+	}
+	if ( $invert ) {
+		$res[1]	=	( $res[1] ) ? false : true;
+	}
 } else {
-	$count	=	(int)JCckDatabase::loadResult( 'SELECT COUNT('.$column.') FROM '.JCckDatabase::quoteName( '#__'.$table ).' WHERE '.JCckDatabase::quoteName( $column ).' = "'.JCckDatabase::escape( $value ).'"'.$and );
-	$res[1]	=	( $count > 0 ) ? false : true;
+	$res[1]	=	true;
 }
-if ( $invert ) {
-	$res[1]	=	( $res[1] ) ? false : true;
-}
-
 // Set
 echo json_encode( $res );
 ?>
