a8f4e5983e706172feaeb45577ed708a2dfd9f8e
Ilya A. Zhulin <ilya.zhulin@hotmail.com>
2024-09-11 16:38:09 +0300

    Update mod_cck_list.php
    Set default value to remove message:
    "*Deprecated*: htmlspecialchars(): Passing null to parameter #1 ($string) of type string is deprecated in */modules/mod_cck_list/mod_cck_list.php on line 138"

--- modules/mod_cck_list/mod_cck_list.php
+++ modules/mod_cck_list/mod_cck_list.php
@@ -135,7 +135,7 @@ if ( ( $show_more == 1 || ( $show_more == 2 && $total ) || ( $show_more == 3 && 
 	}
 }
 $raw_rendering		=	$params->get( 'raw_rendering', 0 );
-$moduleclass_sfx	=	htmlspecialchars( $params->get( 'moduleclass_sfx' ) );
+$moduleclass_sfx	=	htmlspecialchars( $params->get( 'moduleclass_sfx' ), '' );
 $class_sfx			=	( $params->get( 'force_moduleclass_sfx', 0 ) == 1 ) ? $moduleclass_sfx : '';
 require JModuleHelper::getLayoutPath( 'mod_cck_list', $params->get( 'layout', 'default' ) );
 ?>
