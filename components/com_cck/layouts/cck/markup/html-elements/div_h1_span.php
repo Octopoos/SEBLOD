46436dc9643df5fba53a6c54edce66500ae9a09d
Sebastien Heraud <seb@heraud.me>
2025-10-09 17:12:44 +0200

    Update Markup

--- components/com_cck/layouts/cck/markup/html-elements/div_h1_span.php
+++ components/com_cck/layouts/cck/markup/html-elements/div_h1_span.php
@@ -3,5 +3,11 @@ defined( '_JEXEC' ) or die;
 
 $class	=	trim( $displayData['field']->markup_class );
 $class	=	$class ? ' class="'.$class.'"' : '';
+
+if ( JFactory::getApplication()->input->get( 'tmpl' ) === 'raw' ) {
+	echo '<span class="o-hide" data-cck-modal-title>'.$displayData['html'].'</span>';
+
+	return;
+}
 ?>
 <div><h1<?php echo $class; ?>><span><?php echo $displayData['html']; ?></span></h1></div>
