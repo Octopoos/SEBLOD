dd7777ee2503dc23eb012df01f885f742ce9a91e
Sebastien Heraud <seb@heraud.me>
2025-12-01 17:45:17 +0100

    Fix view on WebServices Add-on

--- libraries/cck/_/base/legacy/view/form.php
+++ libraries/cck/_/base/legacy/view/form.php
@@ -22,7 +22,7 @@ class JCckBaseLegacyViewForm extends JViewLegacy
 	{
 		if ( $this->getlayout() == 'delete' ) {
 			$this->prepareDelete();
-		} elseif ( $this->getLayout() == 'edit' || $this->getLayout() == 'edit2' || $this->getLayout() == 'process' ) {
+		} elseif ( $this->getLayout() == 'edit' || $this->getLayout() == 'edit2' || $this->getLayout() == 'call' || $this->getLayout() == 'process' ) {
 			$this->prepareDisplay();
 		}
 
