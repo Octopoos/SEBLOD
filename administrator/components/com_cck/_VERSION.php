8c9f725c53b047f0439ab1259c7d6344b62cf4ca
Sebastien Heraud <seb@heraud.me>
2026-01-19 22:53:38 +0100

    Prepare SEBLOD 6.0.0 Beta6 release

--- administrator/components/com_cck/_VERSION.php
+++ administrator/components/com_cck/_VERSION.php
@@ -17,7 +17,7 @@ final class JCckVersion extends JCckVersionObject
 	
 	public $DEV_LEVEL = '0';
 
-	public $DEV_STATUS = 'Beta5';
+	public $DEV_STATUS = 'Beta6';
 
 	public $API_VERSION = array( 'v3'=>'3.13.0' );
 	
